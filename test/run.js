1aacb5420965b48d5f4cc5d9f356523e95c53a83
adamsko <adam@skobo.pl>
2017-11-09 08:46:01 +0100

    gulp: adding test task runners

--- test/run.js
+++ test/run.js
@@ -1,1 +1,6 @@
-import a0 from './testOne';
+var SystemJS = require('../jspm_packages/system.js');
+var c = require('../config.js');
+
+SystemJS.import('./dist/test/tests.js').catch((err)=>{
+    console.error(err);
+});
