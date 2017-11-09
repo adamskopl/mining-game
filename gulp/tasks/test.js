81f9f68d814bb9da03c7b2ea7a8fdb8acd6cbeaa
adamsko <adam@skobo.pl>
2017-11-09 08:26:40 +0100

    gulp task: test + run-tests

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -7,10 +7,14 @@ function reportChange(event) {
   console.log(`File ${event.path} was ${event.type}, running tasks...`);
 }
 
+// Plans: how to make a proper test running chain? Watching on change of src
+// files is wrong: test should be run after finished transpilation.
+
 gulp.task('run-tests', () => {
   return run('node test/run.js | faucet').exec();
 });
 
+// watching for src changes. not correct.
 gulp.task('test', ['babel-test', 'run-tests'], () => {
   gulp.watch([paths.src, paths.test], ['babel-test', 'run-tests']).on('change', reportChange);
 });
