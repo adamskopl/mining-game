3adde16ce226ec5c1ce3d0dcfafbc0d2511d2c77
adam.skobo <adam@skobo.pl>
2017-11-14 21:23:22 +0100

    bitmaps-manager: initializing bitmaps for different kind of objects types

--- src/bitmaps-manager/bitmaps-manager.js
+++ src/bitmaps-manager/bitmaps-manager.js
@@ -1,31 +1,44 @@
+import CONSTS from '../consts';
+
 const DOT_SIZE = 3;
-let bmd = null;
 let game = null;
 const debug = false;
 
-function reloadBitmap(fieldSize) {
+// MAKE MORE PURE
+function reloadBitmaps(bitmaps, fieldSize) {
   const LINE_LEN = fieldSize - (DOT_SIZE * 2);
-  bmd.resize(fieldSize, fieldSize);
-  bmd.rect(0, 0, fieldSize, fieldSize, '#000000');
-  // border:
-  if (debug) {
-    bmd.rect(DOT_SIZE, 0, LINE_LEN, DOT_SIZE, '#c12023');
-    bmd.rect(fieldSize - DOT_SIZE, DOT_SIZE, DOT_SIZE, LINE_LEN, '#4a9941');
-    bmd.rect(DOT_SIZE, fieldSize - DOT_SIZE, LINE_LEN, DOT_SIZE, '#2f6cbc');
-    bmd.rect(0, DOT_SIZE, DOT_SIZE, LINE_LEN, '#FFFFFF');
-  }
+
+  Object.keys(bitmaps).forEach((k) => {
+    const b = bitmaps[k];
+    b.resize(fieldSize, fieldSize);
+    b.rect(0, 0, fieldSize, fieldSize, CONSTS.BITMAPS[k]);
+    // border:
+    if (debug) {
+      b.rect(DOT_SIZE, 0, LINE_LEN, DOT_SIZE, '#c12023');
+      b.rect(fieldSize - DOT_SIZE, DOT_SIZE, DOT_SIZE, LINE_LEN, '#4a9941');
+      b.rect(DOT_SIZE, fieldSize - DOT_SIZE, LINE_LEN, DOT_SIZE, '#2f6cbc');
+      b.rect(0, DOT_SIZE, DOT_SIZE, LINE_LEN, '#FF');
+    }
+  });
 }
 
 export default {
   init(g) {
     game = g;
-    bmd = g.add.bitmapData(1, 1);
-    game.cache.addBitmapData('testBitmap', bmd);
+    // initialize bitmaps, add them to cache
+    this.bitmaps = {};
+    Object.keys(CONSTS.OBJECT_TYPE).reduce((arr, key) => {
+      arr.push([key, g.add.bitmapData(1, 1)]);
+      return arr;
+    }, []).forEach(([name, bitmap]) => {
+      this.bitmaps[name] = bitmap;
+      game.cache.addBitmapData(name, bitmap);
+    });
   },
   onFieldResize(fieldSize) {
-    reloadBitmap(fieldSize);
+    reloadBitmaps(this.bitmaps, fieldSize);
   },
   getBitmap() {
-    return game.cache.getBitmapData('testBitmap');
+    return game.cache.getBitmapData(CONSTS.OBJECT_TYPE.FILLED);
   },
 };
---
+++ src/consts.js
@@ -0,0 +1,16 @@
+const OBJECT_TYPE = {
+  EMPTY: 'EMPTY',
+  FILLED: 'FILLED',
+  HERO: 'HERO',
+};
+
+const BITMAPS = {
+  [OBJECT_TYPE.EMPTY]: '#000000',
+  [OBJECT_TYPE.FILLED]: '#dd6801',
+  [OBJECT_TYPE.HERO]: '#e5dcdf',
+};
+
+export default {
+  OBJECT_TYPE,
+  BITMAPS,
+};
