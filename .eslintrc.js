09cf4ce2642253be83fb3348d39f83410f516e8b
adam.skobo <adam@skobo.pl>
2017-12-19 22:08:18 +0100

    eslint prefer-rest-params off

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "extends": "airbnb-base",
     "rules": {
-        "import/no-extraneous-dependencies": "off"
+      "import/no-extraneous-dependencies": "off",
+      "prefer-rest-params": "off"
     },
     "plugins": [
         "import"
