41ddfa3a5f853ca8d16eef7563d83ff5b1381a0e
Adam Skobodziński <adam@skobo.pl>
2018-04-04 23:32:18 +0200

    eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
 module.exports = {
-    'extends': 'airbnb-base',
-    'rules': {
-      'import/no-extraneous-dependencies': 'off',
-      'prefer-rest-params': 'off',
-      'max-len': ['error', 80],
-      'no-use-before-define': 'off'
-    },
-    'plugins': [
-        'import'
-    ]
+  'extends': 'airbnb-base',
+  'settings': {
+    'import/resolver': {
+      'jspm': {
+        'moduleDirectory': 'jspm_packages'
+      }
+    }
+  },
+  'rules': {
+    'import/no-extraneous-dependencies': 'off',
+    'prefer-rest-params': 'off',
+    'max-len': ['error', 80],
+    'no-use-before-define': 'off'
+  },
+  'plugins': [
+    'import'
+  ]
 };
