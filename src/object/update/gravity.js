6ed6a925f4632a65f322aeb637673086afc9d6d2
adamsko <adam@skobo.pl>
2018-05-27 20:45:07 +0200

    gravity.js refactoring

--- src/object/update/gravity.js
+++ src/object/update/gravity.js
@@ -6,11 +6,43 @@ import {
 
 const GRAV = {
   vec: new Phaser.Point(0, 1),
-  time: 1200,
+  time: 600,
   fieldsNumber: 50, // arbitrary
 };
 
 export { GRAV, handleGravity };
+
+function handleGravityForTween(o, otherObjects) {
+  let objectsEvents = [];
+  const objectsIntersecting = otherObjects.filter(x => utils.willIntersect(
+    o.$rec,
+    x.$rec,
+    o.tweenObj.posTweened,
+  ));
+  if (objectsIntersecting.length > 0) {
+    objectsEvents = objectsEvents.concat(
+      getGameObjectEventsForIntersection(
+        o,
+        GRAV.vec,
+        objectsIntersecting,
+      ),
+    );
+  }
+  // continue the movement (object may be realigned during events resolve)
+  o.$setPos(o.tweenObj.posTweened);
+  return objectsEvents;
+}
+
+function startTween(o, fieldSize) {
+  o.$startMovement(createTweenObj(
+    o,
+    fieldSize,
+    GRAV.vec,
+    GRAV.fieldsNumber,
+    Phaser.Easing.Cubic.In,
+    GRAV.time,
+  ));
+}
 
 /**
  * @param {number} fieldSize
@@ -18,31 +50,9 @@ export { GRAV, handleGravity };
 function handleGravity(o, otherObjects, fieldSize) {
   let objectsEvents = [];
   if (o.$isTweenRunning()) {
-    const objectsIntersecting = otherObjects.filter(x => utils.willIntersect(
-      o.$rec,
-      x.$rec,
-      o.tweenObj.posTweened,
-    ));
-    if (objectsIntersecting.length > 0) {
-      objectsEvents = objectsEvents.concat(
-        getGameObjectEventsForIntersection(
-          o,
-          GRAV.vec,
-          objectsIntersecting,
-        ),
-      );
-    }
-    // continue the movement (object may be realigned during events resolve)
-    o.$setPos(o.tweenObj.posTweened);
+    objectsEvents = handleGravityForTween(o, otherObjects);
   } else {
-    o.$startMovement(createTweenObj(
-      o,
-      fieldSize,
-      GRAV.vec,
-      GRAV.fieldsNumber,
-      Phaser.Easing.Cubic.In,
-      GRAV.time,
-    ));
+    startTween(o, fieldSize);
   }
   return objectsEvents;
 }
